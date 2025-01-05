3f9f9b561de8cb500448ad71b3eb2101608cf07e
tariq926 <ochiengphidel1@gmail.com>
2025-01-05 15:09:04 +0300

    test

--- script.js
+++ script.js
@@ -16,3 +16,54 @@ navLink.forEach((link) => {
     nav.classList.remove("show");
   });
 });
+
+document.getElementById('project1-highlights').addEventListener('click', function() {
+  window.location.href = 'project1-highlights.html';
+});
+
+document.getElementById('project1-details').addEventListener('click', function() {
+  window.location.href = 'project1-details.html';
+});
+
+document.getElementById('project2-highlights').addEventListener('click', function() {
+  window.location.href = 'project2-highlights.html';
+});
+
+document.getElementById('project2-details').addEventListener('click', function() {
+  window.location.href = 'project2-details.html';
+});
+
+document.getElementById('project3-highlights').addEventListener('click', function() {
+  window.location.href = 'project3-highlights.html';
+});
+
+document.getElementById('project3-details').addEventListener('click', function() {
+  window.location.href = 'project3-details.html';
+});
+
+document.getElementById('project4-highlights').addEventListener('click', function() {
+  window.location.href = 'project4-highlights.html';
+});
+
+document.getElementById('project4-details').addEventListener('click', function() {
+  window.location.href = 'project4-details.html';
+});
+
+document.getElementById('project5-highlights').addEventListener('click', function() {
+  window.location.href = 'project5-highlights.html';
+});
+
+document.getElementById('project5-details').addEventListener('click', function() {
+  window.location.href = 'project5-details.html';
+});
+
+document.getElementById('project6-highlights').addEventListener('click', function() {
+  window.location.href = 'project6-highlights.html';
+});
+
+document.getElementById('project6-details').addEventListener('click', function() {
+  window.location.href = 'project6-details.html';
+});
+document.getElementById('project1-highlights').addEventListener('click', () => {
+  window.location.href = 'index.html#about';
+});
